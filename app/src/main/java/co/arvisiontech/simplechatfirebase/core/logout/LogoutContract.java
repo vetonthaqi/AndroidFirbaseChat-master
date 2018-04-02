28981761f65ece60024c251102cf0384372e65a7
VetonTh <thaqiveton@gmail.com>
2018-04-02 16:45:59 +0200

    Initial commit

---
+++ app/src/main/java/co/arvisiontech/simplechatfirebase/core/logout/LogoutContract.java
@@ -0,0 +1,25 @@
+package co.arvisiontech.simplechatfirebase.core.logout;
+
+
+
+public interface LogoutContract {
+    interface View {
+        void onLogoutSuccess(String message);
+
+        void onLogoutFailure(String message);
+    }
+
+    interface Presenter {
+        void logout();
+    }
+
+    interface Interactor {
+        void performFirebaseLogout();
+    }
+
+    interface OnLogoutListener {
+        void onSuccess(String message);
+
+        void onFailure(String message);
+    }
+}
